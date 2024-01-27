543dd546e2707188aecde471868bce637c4ddc52
GeekyPBhaumik <134827207+GeekyPBhaumik@users.noreply.github.com>
2024-01-27 12:19:21 +0530

    Add files via upload

---
+++ ValidPalindromeProblem.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+//https://leetcode.com/problems/valid-palindrome/description/
+public class ValidPalindromeProblem {
+
+    static boolean isValidPalin(String s){
+        s = s.replaceAll("[^a-zA-Z0-9]","");
+        s = s.toLowerCase();
+        int i = 0;
+        int j = s.length()-1;
+        while(i<j){
+            if(s.charAt(i)!=s.charAt(j)){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        if(isValidPalin(s))
+           System.out.println("Valid Palindrome");
+        else
+            System.out.println("Invalid Palindrome");
+    }
+}
