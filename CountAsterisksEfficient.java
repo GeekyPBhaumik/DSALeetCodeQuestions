16534cfb0a0320ea6da2fbc3fe88dee9741974db
GeekyPBhaumik <134827207+GeekyPBhaumik@users.noreply.github.com>
2024-01-28 11:55:14 +0530

    Add files via upload

---
+++ CountAsterisksEfficient.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class CountAsterisksEfficient {
+    static int countAsterisk(String s) {
+        //l|*e*et|c**o|*de|, l , *e*et , c**o , *de
+        String strArr[] = s.split("\\|");
+        int countAsterisks = 0;
+        for (int i = 0; i < strArr.length; i++) {
+            if (i % 2 == 0) {
+                for (char ch : strArr[i].toCharArray()) {
+                    if (ch == '*') {
+                        countAsterisks++;
+                    }
+                }
+            }
+        }
+        return  countAsterisks;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.next();
+        System.out.println(countAsterisk(s));
+    }
+}
